6d4d1469ca47706c39f7cc7148953c39499e801a
José Gabriel González <jgg032899@gmail.com>
2021-05-29 23:53:22 -0400

    Player animations

--- Player.gd
+++ Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const GRAVITY = 20
+const GRAVITY = 35
 const MOVE_SPEED = 200
-const JUMP_FORCE = -500
+const JUMP_FORCE = -400
 
 var motion = Vector2()
 
@@ -11,13 +11,20 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = MOVE_SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -MOVE_SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("run")
 	else:
+		$Sprite.play("idle")
 		motion.x = 0
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y = JUMP_FORCE;
+			motion.y = JUMP_FORCE
+	else:
+		$Sprite.play("jump")
 	
 	motion = move_and_slide(motion, Vector2.UP)
