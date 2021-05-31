543c4399faa9bacabe17e6ec1574bff6af40c99d
José Gabriel González <jgg032899@gmail.com>
2021-05-31 01:07:44 -0400

    Changing between worlds
    - Smooth player movement

--- Player.gd
+++ Player.gd
@@ -1,30 +1,40 @@
 extends KinematicBody2D
 
 const GRAVITY = 35
-const MOVE_SPEED = 200
+const ACCELERATION = 50
+const MAX_SPEED = 200
 const JUMP_FORCE = -400
 
 var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += 10
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = MOVE_SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -MOVE_SPEED
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("run")
 	else:
 		$Sprite.play("idle")
-		motion.x = 0
+		friction = true
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_FORCE
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		$Sprite.play("jump")
+		if motion.y < 0:
+			$Sprite.play("jump")
+		else:
+			$Sprite.play("fall")
+		
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.05)
 	
 	motion = move_and_slide(motion, Vector2.UP)
